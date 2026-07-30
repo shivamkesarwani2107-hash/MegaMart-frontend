9c1eeae6c9b1a9af439a5a09190bce821b98f20c
Shivam Kesarwani <shivamkesarwani2107@gmail.com>
2026-07-31 00:45:50 +0530

    Initial frontend setup

---
+++ src/frontend/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        MegaMart Header
+      </h1>
+    </div>
+  );
+}
+
---
+++ src/frontend/home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        MegaMart
+      </h1>
+    </div>
+  );
+}
+
